f104fb1887c0d6c1f229172343d3287d1c96de39
Mohammad Saheer Padinharayil <saheerpadinharayil@gmail.com>
2021-03-14 23:21:56 -0500

    Initial commit
    Conversions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,10 +21,14 @@ public class Main {
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("myDoubleValue = " + myDoubleValue);
 
-
         double numberOfPounds = 200d;
         double convertedKilograms = numberOfPounds * 0.4539237d;
         System.out.println("Converted Kilograms =" + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
 	
     }
 }
